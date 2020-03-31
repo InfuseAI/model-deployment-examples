3cb55251862c7c4b55cf940fadc70682e3c688d7
ericy <ericy@infuseai.io>
2020-03-31 10:59:45 +0800

    Add mnist example of keras
    Signed-off-by: Eric Yang <ericy@infuseai.io>

---
+++ keras_mnist/MyModel.py
@@ -0,0 +1,20 @@
+from keras.models import load_model
+from PIL import Image
+from io import BytesIO
+import numpy as np
+
+class MyModel(object):
+    def __init__(self):
+        print('calling init()...')
+        self.model = load_model('keras-mnist.h5')
+        self.model._make_predict_function()
+
+    def predict(self, request, names=None, meta=None):
+        print('calling predict()...')
+        imageStream = BytesIO(request)
+        image = Image.open(imageStream).resize((28, 28)).convert('L')
+
+        data = np.asarray(image)
+        data = np.expand_dims(data, axis=0)
+        data = np.expand_dims(data, axis=-1)
+        return self.model.predict(data)
---
+++ keras_mnist/query_by_physical_image.sh
@@ -0,0 +1,1 @@
+curl -F 'binData=@../tensorflow2_mnist/first_image.jpg' http://0.0.0.0:5000/predict
