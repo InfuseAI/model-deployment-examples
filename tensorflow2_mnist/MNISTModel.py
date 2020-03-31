dd8079372e3162f4711632a192a456f07bee44a0
Jack Pan <jackpan@infuseai.io>
2020-03-31 15:59:47 +0800

    Modify tensorflow2 example
    Signed-off-by: Jack Pan <jackpan@infuseai.io>

--- tensorflow2_mnist/MNISTModel.py
+++ tensorflow2_mnist/MNISTModel.py
@@ -1,26 +1,11 @@
-import numpy as np
 import tensorflow as tf
-from PIL import Image
-import requests
-from io import BytesIO
 
 class MNISTModel:
     def __init__(self):
         self._model = tf.keras.models.load_model('1')
 
-    def predict_raw(self, request):
-        data = request.get("data", {})
-        input_type = request.get("data", {}).get("type")
-        input_data = None
-        if input_type == "tensor":
-            input_data = data.get("input")
-        if input_type == "url":
-            response = requests.get(data.get("input"))
-            img = Image.open(BytesIO(response.content))
-            input_data = tf.keras.preprocessing.image.img_to_array(img, data_format="channels_first")
-            input_data = input_data / 255.0
-        input_data = np.array(input_data, dtype=np.float64)
-        output = self._model.predict(input_data)
-        predicted_number = tf.math.argmax(output[0], axis=0)
-        return {"predicted_number": predicted_number.numpy().tolist(), "probability": output.tolist()}
-
+    def predict(self, X, feature_names=None, meta=None):
+        output = self._model.predict(X)
+        probability = output[0]
+        predicted_number = tf.math.argmax(probability)
+        return {"predicted_number": predicted_number.numpy().tolist(), "probability": probability.tolist()}
---
+++ tensorflow2_mnist/README.md
@@ -0,0 +1,17 @@
+# Tensorflow 2 Example
+
+## How to build the docker image
+
+s2i build . seldonio/seldon-core-s2i-python3:0.18 tensorflow2-mnist
+
+## How to run the docker image 
+
+docker run -p 5000:5000 --rm tensorflow2-mnist
+
+## How to submit a post request
+
+./query_by_tensor.sh
+
+## Sturcture
+
+The training code is under train.py. Serving code is under MNISTModel.py. Model files are under folder `1` (the number is the version of the model which is a convention of tfserving).
