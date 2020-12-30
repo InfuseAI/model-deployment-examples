c5f7c746098387c5d8f26e83f337cd821e8d0e20
ericy <ericy@infuseai.io>
2020-12-30 13:47:32 +0800

    update detection result with aspect ratio
    Signed-off-by: ericy <ericy@infuseai.io>

--- darknet_yolov4/MyModel.py
+++ darknet_yolov4/MyModel.py
@@ -1,7 +1,6 @@
 import darknet
 import cv2
 import numpy as np
-from io import BytesIO
 
 class MyModel(object):
     def __init__(self, model_uri ='/mnt/models'):
@@ -32,11 +31,38 @@ class MyModel(object):
         width = darknet.network_width(network)
         height = darknet.network_height(network)
         darknet_image = darknet.make_image(width, height, 3)
+        aspectRatio = [image.shape[0]/width, image.shape[1]/height]
+
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image_resized = cv2.resize(image_rgb, (width, height),
                                 interpolation=cv2.INTER_LINEAR)
         darknet.copy_image_from_bytes(darknet_image, image_resized.tobytes())
         detections = darknet.detect_image(network, class_names, darknet_image, thresh=thresh)
         darknet.free_image(darknet_image)
-        image = darknet.draw_boxes(detections, image_resized, class_colors)
-        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), detections
+        image = self.cvDrawBoxes(detections, image, class_colors, aspectRatio)
+        return image, detections
+
+    def cvDrawBoxes(self, detections, img, class_colors, aspectRatio):
+        for detection in detections:
+            x, y, w, h = detection[2][0],\
+                detection[2][1],\
+                detection[2][2],\
+                detection[2][3]
+
+            xmin = int(aspectRatio[1]*round(x - (w / 2)))
+            xmax = int(aspectRatio[1]*round(x + (w / 2)))
+            ymin = int(aspectRatio[0]*round(y - (h / 2)))
+            ymax = int(aspectRatio[0]*round(y + (h / 2)))
+
+            pt1 = (xmin, ymin)
+            pt2 = (xmax, ymax)
+            cv2.rectangle(img, pt1, pt2, class_colors[detection[0]], 1)
+            cv2.putText(img,
+                        f"{detection[0]}[{detection[1]}]",
+                        (pt1[0], pt1[1] - 5),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        class_colors[detection[0]],
+                        2
+            )
+        return img
